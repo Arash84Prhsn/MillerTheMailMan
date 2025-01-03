6243bbc9ca02cb0a248d9259fd6457ccd4e7e1d6
Arash84Prhsn <poorhasaniarash@gmail.com>
2025-01-03 10:54:57 +0330

    s

--- src/EmployeeTree.java
+++ src/EmployeeTree.java
@@ -1,3 +1,48 @@
 public class EmployeeTree {
 
+// Private Vertex class.---------------------------------------------------------- 
+    private class Vertex {
+
+        // Vertex attributes:
+        String data = "";
+        Vertex[] children = null;
+        Vertex parent = null;
+
+        // Vertex constructors:
+        public Vertex(){}
+        public Vertex(String data) {this.data = data;}
+        public Vertex(String data, Vertex[] children, Vertex parent) {
+            this.data = data;
+            this.children = children;
+            this.parent = parent;
+        }
+
+        // Getter methods:
+        public String getData() {return data;}
+        public Vertex[] getChildren() {return children;}
+        public Vertex getParent() {return parent;}
+
+        // Setter methods:
+        public void setData(String data) {this.data = data;}
+        public void setChildren(Vertex[] children) {this.children = children;}
+        public void setParent(Vertex parent) {this.parent = parent;}
+
+    }
+// End of Vertex class.------------------------------------------------------------
+
+    private Vertex root = null;
+    private int size = 0;
+
+    public EmployeeTree(){}
+
+    public void insert(String[] newChildren) {
+
+        
+
+    }
+
+
+
+
 }
+
