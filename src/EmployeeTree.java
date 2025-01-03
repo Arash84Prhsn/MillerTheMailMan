c6e3b5157d0620f7d39e24ac8fa6e403c4b804b1
Arash84Prhsn <poorhasaniarash@gmail.com>
2025-01-03 11:49:46 +0330

    s

--- src/EmployeeTree.java
+++ src/EmployeeTree.java
@@ -11,6 +11,7 @@ public class EmployeeTree {
         // Vertex constructors:
         public Vertex(){}
         public Vertex(String data) {this.data = data;}
+        public Vertex(String data, Vertex parent) {this.data = data; this.parent = parent;}
         public Vertex(String data, Vertex[] children, Vertex parent) {
             this.data = data;
             this.children = children;
@@ -35,9 +36,41 @@ public class EmployeeTree {
 
     public EmployeeTree(){}
 
-    public void insert(String[] newChildren) {
+    public Vertex getRootVertex(){return this.root;}
+    public String getRootData() {return this.getRootVertex().getData();}
+    public int getSize() {return this.size;}
+
+    public void setRoot(Vertex newRoot) {this.root = newRoot;}
+
+
+    public boolean isEmpty() {return size==0;}
+    
+    public void insert(String[] newInsert) {
+
+        String[] childern = new String[newInsert.length-1];
+
+        for (int i = 1; i < newInsert.length; i++)
+            childern[i-1] = newInsert[i];
+
+        if (this.isEmpty()) {
+            
+            Vertex newRoot = new Vertex(newInsert[0]);
+            
+            Vertex[] childrenVertexes = new Vertex[childern.length];
+
+            for (int j = 0; j < childern.length; j++) {
+                childrenVertexes[j] = new Vertex(childern[j], newRoot);
+                size++;
+            }
+
+            newRoot.setChildren(childrenVertexes);
+            this.setRoot(newRoot);
+            size++;
+            return;
+        }
 
         
+
 
     }
 
