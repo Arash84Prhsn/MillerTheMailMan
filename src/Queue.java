42c453e74831886b19b5d4f915172a0fbd364932
Arash84Prhsn <poorhasaniarash@gmail.com>
2025-01-03 11:55:12 +0330

    s

---
+++ src/Queue.java
@@ -0,0 +1,67 @@
+public class Queue<T> {
+
+    private class Node<T> {
+
+        private T element;
+        private Node<T> next;
+
+        public Node(T element, Node<T> next) {
+            this.element = element;
+            this.next = next;
+        }
+
+        public void setElement(T element) {this.element = element;}
+        public void setNext(Node<T> next) {this.next = next;}
+
+        public T getElement() {return element;}
+        public Node<T> getNext() {return next;}
+
+    }
+
+    private Node<T> head = null;
+    private Node<T> tail = null;
+    private int size = 0;
+
+    public Queue(){}
+
+    public boolean isEmpty() {return size==0;}
+
+    public T peek() {
+        if (isEmpty()) return null;
+
+        return head.getElement();
+    }
+
+    public void enqueue(T newElement) {
+        
+        Node<T> newNode = new Node(newElement, null);
+
+        if (isEmpty()) {
+            
+            head = newNode;
+            tail = newNode;
+            head.setNext(tail);  
+            size++; 
+            return;
+        }
+
+        tail.setNext(newNode);
+        tail = newNode;
+        size++;
+    }
+
+    public T dequeue() {
+        
+        if (isEmpty()) return null;
+
+        T item = head.getElement();
+        head = head.next;
+        size--;
+        return item;
+    }
+
+    public void enqueue(T[] items) {
+        for (T i : items)
+            this.enqueue(i);
+    }
+}
